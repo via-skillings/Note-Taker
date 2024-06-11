e8e093a11b6ff83b8affcd193eaafae0129bf132
Via Skillings <viaskillings@gmail.com>
2024-06-10 20:14:01 -0500

    connection

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const sequlize = require('./config/connection');
 
 // Serve static files from the 'public' directory
 app.use(express.json());
